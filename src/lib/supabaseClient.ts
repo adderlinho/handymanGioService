6fe729c7cd7aa78e0e573b8da402702e1f99a4b9
adderlinho <addersinay@gmail.com>
2025-11-24 17:44:29 -0600

    Fix Supabase anon key

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL || 'https://ywioqvsdhsygzlkgedem.supabase.co';
-const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aW9xdnNkaHN5Z3psa2dlZGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NzE5NzcsImV4cCI6MjA1MDU0Nzk3N30.H6QPPDdw8V9d3SxE1OkONg_ANQH8y7BqQJxVZQJxVZQ';
+const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aW9xdnNkaHN5Z3psa2dlZGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4NjYxMDgsImV4cCI6MjA3OTQ0MjEwOH0.R646G2q3bgW45WJMeEpL7bPIthuvnUlN_BbqC6ucFj8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
